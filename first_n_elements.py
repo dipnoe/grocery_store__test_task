3c0aa62cc1cf94da4ba72a5afe91816e4f1e7cc8
Vagner Elena <dipnoe@gmail.com>
2024-03-06 16:42:30 +0300

    fix: fix func

--- first_n_elements.py
+++ first_n_elements.py
@@ -2,9 +2,11 @@ def output_n_elements(n: int) -> str:
     nums = ''
     for i in range(1, n + 1):
         nums += (str(i) * i)
-    return nums
+        if len(nums) >= n:
+            return nums[:n]
 
 
 if __name__ == "__main__":
-    n = int(input())
+    n = int(input('Введите n: '))
     print(output_n_elements(n))
+
